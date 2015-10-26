f35683f3b82070b5cc0862c97c5f9328a9a030a8
gauravsobti1 <gauravsobti1@gmail.com>
2015-10-26 15:28:00 +0530

    Final Commit

---
+++ football_scores_super_duo-Revision1/app/src/main/java/barqsoft/footballscores/FootballScoresWidgetProvider.java
@@ -0,0 +1,28 @@
+package barqsoft.footballscores;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by hp on 10/22/2015.
+ */
+public class FootballScoresWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+    }
+}
